da19cf07beaa254776f094d2c9614a6a855b97c8
Lingyi Zhou <reatured@Lingyis-MacBook-Pro.local>
2020-03-08 12:14:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/takehomeassignment6_lingyiz/DisplayWindow.java
@@ -0,0 +1,14 @@
+package com.example.takehomeassignment6_lingyiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_window);
+    }
+}
---
+++ app/src/main/java/com/example/takehomeassignment6_lingyiz/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.takehomeassignment6_lingyiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1, e2, e3;
+    float f1, f2, f3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void phase(View view) {
+
+        getValue();
+
+        Intent m = new Intent(this, DisplayWindow.class);
+        startActivity(m);
+        m.putExtra("Amount", f1);
+        m.putExtra("Tax", f2);
+        m.putExtra("Tip", f3);
+    }
+
+    private void getValue() {
+        e1 = (EditText)findViewById(R.id.e1);
+        f1 = Float.parseFloat(e1.getText().toString());
+        e2 = (EditText)findViewById(R.id.e2);
+        f2 = Float.parseFloat(e2.getText().toString());
+        e3 = (EditText)findViewById(R.id.e3);
+        f3 = Float.parseFloat(e3.getText().toString());
+    }
+}
